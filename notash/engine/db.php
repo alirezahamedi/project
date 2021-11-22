cbdd26c71333f16fd142c2a22f7491cbcfe5e333
paradoux <93842157+alirezahamedi@users.noreply.github.com>
2021-11-22 16:23:55 -0500

    Delete notash directory

--- notash/engine/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = mysqli_connect('localhost', 'root', '', 'alireza_users');
-session_start();
-if (isset($_SESSION['loggedin'])) {
-    $user_username = $_SESSION['loggedin'];
-} else {
-    $user_username = "";
-}
-?>
